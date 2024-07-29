0e0279ab4c03e38c93efc3fedd522591d39c0156
qalqa <andreybas04@gmail.com>
2024-07-29 18:33:22 +0300

    feat: added search comp + simple routing

--- wone-hotels/src/app/app.component.ts
+++ wone-hotels/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
 import { HeaderComponent } from './components/header/header.component';
+import { SearchComponent } from './pages/search/search.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AboutComponent, HeaderComponent],
+  imports: [RouterOutlet, AboutComponent, HeaderComponent, SearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- wone-hotels/src/app/app.routes.ts
+++ wone-hotels/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
+import { SearchComponent } from './pages/search/search.component';
 
 export const routes: Routes = [
   {
     path: 'About',
     component: AboutComponent,
+  },
+  {
+    path: 'Search',
+    component: SearchComponent,
   },
 ];

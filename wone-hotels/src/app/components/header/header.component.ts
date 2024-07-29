1ae0d7b37de9a136b48d479509d4c228ffff2b75
qalqa <andreybas04@gmail.com>
2024-07-29 16:58:31 +0300

    feat: angular envelope tiered-menu

--- wone-hotels/src/app/components/header/header.component.ts
+++ wone-hotels/src/app/components/header/header.component.ts
@@ -1,87 +1,11 @@
 import { Component } from '@angular/core';
-import { MenuItem } from 'primeng/api';
-import { TieredMenuModule } from 'primeng/tieredmenu';
-import { ButtonModule } from 'primeng/button';
+import { TieredMenuComponent } from '../../ui/components/tiered-menu/tiered-menu.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [TieredMenuModule, ButtonModule],
+  imports: [TieredMenuComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-  items: MenuItem[] | undefined;
-
-  ngOnInit() {
-    this.items = [
-      {
-        label: 'File',
-        icon: 'pi pi-file',
-        items: [
-          {
-            label: 'New',
-            icon: 'pi pi-plus',
-            items: [
-              {
-                label: 'Document',
-                icon: 'pi pi-file',
-              },
-              {
-                label: 'Image',
-                icon: 'pi pi-image',
-              },
-              {
-                label: 'Video',
-                icon: 'pi pi-video',
-              },
-            ],
-          },
-          {
-            label: 'Open',
-            icon: 'pi pi-folder-open',
-          },
-          {
-            label: 'Print',
-            icon: 'pi pi-print',
-          },
-        ],
-      },
-      {
-        label: 'Edit',
-        icon: 'pi pi-file-edit',
-        items: [
-          {
-            label: 'Copy',
-            icon: 'pi pi-copy',
-          },
-          {
-            label: 'Delete',
-            icon: 'pi pi-times',
-          },
-        ],
-      },
-      {
-        label: 'Search',
-        icon: 'pi pi-search',
-      },
-      {
-        separator: true,
-      },
-      {
-        label: 'Share',
-        icon: 'pi pi-share-alt',
-        items: [
-          {
-            label: 'Slack',
-            icon: 'pi pi-slack',
-          },
-          {
-            label: 'Whatsapp',
-            icon: 'pi pi-whatsapp',
-          },
-        ],
-      },
-    ];
-  }
-}
+export class HeaderComponent {}
---
+++ wone-hotels/src/app/ui/components/tiered-menu/tiered-menu.component.ts
@@ -0,0 +1,85 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { TieredMenuModule } from 'primeng/tieredmenu';
+
+@Component({
+  selector: 'app-tiered-menu',
+  standalone: true,
+  imports: [TieredMenuModule],
+  templateUrl: './tiered-menu.component.html',
+  styleUrl: './tiered-menu.component.scss',
+})
+export class TieredMenuComponent {
+  items: MenuItem[] | undefined;
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'File',
+        icon: 'pi pi-file',
+        items: [
+          {
+            label: 'New',
+            icon: 'pi pi-plus',
+            items: [
+              {
+                label: 'Document',
+                icon: 'pi pi-file',
+              },
+              {
+                label: 'Image',
+                icon: 'pi pi-image',
+              },
+              {
+                label: 'Video',
+                icon: 'pi pi-video',
+              },
+            ],
+          },
+          {
+            label: 'Open',
+            icon: 'pi pi-folder-open',
+          },
+          {
+            label: 'Print',
+            icon: 'pi pi-print',
+          },
+        ],
+      },
+      {
+        label: 'Edit',
+        icon: 'pi pi-file-edit',
+        items: [
+          {
+            label: 'Copy',
+            icon: 'pi pi-copy',
+          },
+          {
+            label: 'Delete',
+            icon: 'pi pi-times',
+          },
+        ],
+      },
+      {
+        label: 'Search',
+        icon: 'pi pi-search',
+      },
+      {
+        separator: true,
+      },
+      {
+        label: 'Share',
+        icon: 'pi pi-share-alt',
+        items: [
+          {
+            label: 'Slack',
+            icon: 'pi pi-slack',
+          },
+          {
+            label: 'Whatsapp',
+            icon: 'pi pi-whatsapp',
+          },
+        ],
+      },
+    ];
+  }
+}

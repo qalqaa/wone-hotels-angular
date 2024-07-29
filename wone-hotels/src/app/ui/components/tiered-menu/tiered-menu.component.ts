74c18f58dfe280194e26af954ffaa59d1b4d6967
qalqa <andreybas04@gmail.com>
2024-07-29 18:03:27 +0300

    feat: changed styles for burger menu

--- wone-hotels/src/app/ui/components/tiered-menu/tiered-menu.component.ts
+++ wone-hotels/src/app/ui/components/tiered-menu/tiered-menu.component.ts
@@ -14,71 +14,31 @@ export class TieredMenuComponent {
   ngOnInit() {
     this.items = [
       {
-        label: 'File',
-        icon: 'pi pi-file',
-        items: [
-          {
-            label: 'New',
-            icon: 'pi pi-plus',
-            items: [
-              {
-                label: 'Document',
-                icon: 'pi pi-file',
-              },
-              {
-                label: 'Image',
-                icon: 'pi pi-image',
-              },
-              {
-                label: 'Video',
-                icon: 'pi pi-video',
-              },
-            ],
-          },
-          {
-            label: 'Open',
-            icon: 'pi pi-folder-open',
-          },
-          {
-            label: 'Print',
-            icon: 'pi pi-print',
-          },
-        ],
+        label: 'Day Spa',
       },
       {
-        label: 'Edit',
-        icon: 'pi pi-file-edit',
-        items: [
-          {
-            label: 'Copy',
-            icon: 'pi pi-copy',
-          },
-          {
-            label: 'Delete',
-            icon: 'pi pi-times',
-          },
-        ],
+        label: 'Spa-зона',
       },
       {
-        label: 'Search',
-        icon: 'pi pi-search',
+        label: 'Фитнес центр',
       },
       {
-        separator: true,
+        label: 'Рестораны',
       },
       {
-        label: 'Share',
-        icon: 'pi pi-share-alt',
-        items: [
-          {
-            label: 'Slack',
-            icon: 'pi pi-slack',
-          },
-          {
-            label: 'Whatsapp',
-            icon: 'pi pi-whatsapp',
-          },
-        ],
+        label: 'Детский клуб',
+      },
+      {
+        label: 'Прайс-лист',
+      },
+      {
+        label: 'О нас',
+      },
+      {
+        label: 'Акции и новости',
+      },
+      {
+        label: 'Правовая документация',
       },
     ];
   }
